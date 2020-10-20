b762acb66fa3d414a758c7e5224ca0fc3cc983cd
DCXACN <DCXACN@global.agcocorp.com>
2020-10-20 21:58:19 +0530

    code updated

--- src/Components/BasicStructures/Right/Routes/ContactRoute.js
+++ src/Components/BasicStructures/Right/Routes/ContactRoute.js
@@ -1,45 +1,19 @@
-/*
-MIT License
-
-Copyright (c) 2020 Fayaz Bin Salam
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-import React from 'react';
+import React from "react";
 import Grid from "@material-ui/core/Grid";
-import {useOvermind} from "../../../../Others/OvermindHelper";
-import {getTextAndListGrid} from "../../../../Others/GlobalMethods";
-import {contactInfos} from "../../../../Others/GlobalVariables";
+import { useOvermind } from "../../../../Others/OvermindHelper";
+import { getTextAndListGrid } from "../../../../Others/GlobalMethods";
+import { contactInfos } from "../../../../Others/GlobalVariables";
 
 const ContactRoute = () => {
-    const {state, actions} = useOvermind()
+  const { state } = useOvermind();
 
-    return (
-        <Grid spacing={1} container xs item>
-            {
-                contactInfos.map((item) => {
-                    return getTextAndListGrid(item, state.belowSm ? 90 : 80);
-                })
-            }
-        </Grid>
-    );
+  return (
+    <Grid spacing={1} container xs item>
+      {contactInfos.map((item) => {
+        return getTextAndListGrid(item, state.belowSm ? 90 : 80);
+      })}
+    </Grid>
+  );
 };
 
 export default ContactRoute;
